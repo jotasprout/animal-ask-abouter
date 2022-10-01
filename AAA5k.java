ebcb1bcb357e94be733d6be8daf963e2eff82eee
Autumn Sprout <jotasprout@gmail.com>
2022-10-01 01:03:28 -0400

    we're cruising now

---
+++ AAA5k.java
@@ -0,0 +1,10 @@
+public class AAA5k {
+
+    public int questionsLeft = 10;
+    
+
+    public static void askQuestion(int questionsLeft)
+    {
+        questionsLeft--;
+    }
+}
--- Elimination.java
+++ Elimination.java
@@ -1,6 +1,37 @@
-public class Snark {
+public class Elimination {
+
+    String kingdom;
+    String phylum;
+    String datClass;
+    String order;
+    String family;
+    String genus;
+    String species;
     
-    System.out.println("question? ");
+    System.out.println("I've narrowed it down to the kingdom of " + kingdom + ". Wanna give up yet?");
+    System.out.println("I've narrowed it down to the phylum of " + phylum + ". Wanna give up yet?");
+    System.out.println("I've narrowed it down to the datClass of " + datClass + ". Wanna give up yet?");
+    System.out.println("I've narrowed it down to the order of " + order + ". Wanna give up yet?");
+    System.out.println("I've narrowed it down to the family of " + family + ". Wanna give up yet?");
+    System.out.println("I've narrowed it down to the genus of " + genus + ". Wanna give up yet?");
+    System.out.println("I've narrowed it down to the species of " + species + ". Wanna give up yet?");
+
+    System.out.println("Does it have webbed feet? ");
+        
+    System.out.println("Does it have feathers? ");
+    System.out.println("Does it have wings? ");
+    System.out.println("Does it have fur/hair? ");
+    System.out.println("Does it have scales? ");
+
+    // ENVIRONMENT
+    System.out.println("Does it live in the water? ");
+    System.out.println("Does it live on land? ");
+
+    // ABILITIES
+    System.out.println("Can it generate its own body heat? ");
+    System.out.println("Can it fly? ");
+    System.out.println("Does it lay eggs? ");
+    
     System.out.println("question? ");
     System.out.println("question? ");
     System.out.println("question? ");
